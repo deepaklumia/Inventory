79dc3ac897549a417329deafb270cbe37b7eb1fa
deepaklumia <deepakkumar12071998@gmail.com>
2023-02-09 00:49:38 +0530

    add exported function in router

--- src/router/router.js
+++ src/router/router.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const { addInventory } = require('../collection/addInventory');
+const { getInventoryById } = require('../collection/getInventoryById');
+const { getInventory } = require('../collection/getInventory')
+const { updateInventory } = require('../collection/updateInventory');
+const { deleteInventory } = require('../collection/deleteInventory');
 
-router.get('/', function (req, res){
+router.get('/', function (req, res) {
       res.send('server is running on port 3000');
 })
 
-
+router.get('/inventory', getInventory);
+router.get('/inventory/:id', getInventoryById);
+router.post('/inventory', addInventory);
+router.put('/inventory/:id', updateInventory);
+router.delete('/inventory/:id', deleteInventory);
 module.exports = router;
