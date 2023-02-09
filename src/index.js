fd7d50ca2a809f3ee57d8e2b3f0cd88e25053a01
deepaklumia <deepakkumar12071998@gmail.com>
2023-02-10 00:34:37 +0530

    import cors package

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const router = require('./router/router');
 const { default: mongoose } = require('mongoose');
-
+app.use(cors());
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const mongooseUrl = "mongodb+srv://deepak98:deepaklumia@book.4bu11tl.mongodb.net/Inventory"
@@ -13,6 +15,6 @@ mongoose.connect(mongooseUrl, { useNewUrlParser: true })
       .catch((err) => { console.log(err); });
 
 app.use('/', router);
-app.listen(process.env.PORT || 3000, () => {
-      console.log("Express server listening on port " + (process.env.PORT || 3000));
+app.listen(process.env.PORT || 5000, () => {
+      console.log("Express server listening on port " + (process.env.PORT || 5000));
 });
